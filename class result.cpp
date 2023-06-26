fa0e00d0fa717227b09fafbec0a7abd19830a128
nikhil <nickahir0786@email.com>
2023-06-26 10:04:56 +0530

    result.cpp

---
+++ class result.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+class result{
+    private:
+    int maths,english,gujrati,total;
+    float pr;
+    public:
+    void setter(){
+     cout<<"enter a maths marks:";
+     cin>>maths;
+     cout<<"enter a english marks:";
+     cin>>english;
+     cout<<"enter a gujrati marks:";
+     cin>>gujrati;
+    }
+    void cal(){
+        total=maths+english+gujrati;
+        pr=float(total)/300*100;
+     }
+     void print(){
+     cout<<"total is :"<<total<<endl;
+     cout<<"pr is :"<<pr;
+
+     };
+
+     int main(){
+      result x;
+      x.setter();
+      x.cal();
+      x.print();
+
+     }
+     
+     
+}
+     
+   
+
+
+   
+
+   
+
+
+
