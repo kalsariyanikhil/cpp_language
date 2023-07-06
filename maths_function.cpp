e0d233a952be10b38c93b1248e24d0a6085f5459
nikhil <nickahir0786@email.com>
2023-07-06 10:07:27 +0530

    basic

---
+++ maths_function.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<math.h>
+#include<stdlib.h>
+using namespace std;
+
+        int main()
+      {
+        cout<<"Ans is :"<<ceil(3.2)<<endl;
+        cout<<"Ans is :"<<ceil(-3.8)<<endl<<endl;
+
+        cout<<"Ans is :"<<floor(3.2)<<endl;
+        cout<<"Ans is :"<<floor(-3.8)<<endl<<endl;
+
+        cout<<"Ans is :"<<round(13.2)<<endl;
+        cout<<"Ans is :"<<round(-3.8)<<endl<<endl;
+
+        cout<<"Ans is :"<<abs(13.2)<<endl;
+        cout<<"Ans is :"<<abs(-3.8)<<endl<<endl;
+
+        cout<<"Ans is :"<<sqrt(16)<<endl;
+        cout<<"Ans is :"<<sqrt(5)<<endl<<endl;
+
+        cout<<"Ans is :"<<pow(2,6)<<endl<<endl;
+
+        cout<<"Ans is :"<<rand()%10<<endl<<endl;
+     
+      }
+                                                                   
